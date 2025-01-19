86a6324f09440fc8322c1b077aa7c808b0d1e650
devanshu <devanshuchatterjee1@gmail.com>
2025-01-19 08:59:44 +0530

    .

--- src/main/java/com/example/demo/Cont/RestCont.java
+++ src/main/java/com/example/demo/Cont/RestCont.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import com.example.demo.Repo.createRoomConnect;
 
 
 @RestController
+@CrossOrigin
 public class RestCont {
     
     @Autowired
